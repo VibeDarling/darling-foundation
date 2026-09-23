3d698aa8f722973060eaec763d4f048333cae404
Cristian Magherusan-Stanciu <cristi@leanercloud.com>
2026-09-23 20:48:13 +0200

    Foundation: implement NSUserActivity and NSUserActivityDelegate
    NSUserActivity was an empty interface whose every selector was
    forwarded to an NSLog stub, and NSUserActivityDelegate was not declared
    at all, so Swift code using activityType, needsSave or the delegate
    protocol (OpenSwiftUI's SceneBridge) failed to compile.
    
    Declare the public NSUserActivity API and NSUserActivityDelegate, and
    implement them locally: becomeCurrent makes the activity the process's
    current one, resignCurrent/invalidate clear it, and a current activity
    with needsSave set gets one coalesced main-queue save that calls
    userActivityWillSave: and clears needsSave. The delegate is weak.
    Darling has no Handoff service, so continuation streams report
    NSFeatureUnsupportedError.
    
    Test: tests/nsuseractivity.swift.
    
    Refs VibeDarling/darling#118

--- include/Foundation/NSUserActivity.h
+++ include/Foundation/NSUserActivity.h
@@ -17,9 +17,57 @@
  along with Darling.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#import <Foundation/Foundation.h>
+#import <Foundation/NSObject.h>
+
+@class NSString, NSDictionary, NSSet<ObjectType>, NSURL, NSDate, NSError, NSInputStream, NSOutputStream;
+@protocol NSUserActivityDelegate;
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef NSString *NSUserActivityPersistentIdentifier;
 
 FOUNDATION_EXPORT NSString * const NSUserActivityTypeBrowsingWeb;
 
 @interface NSUserActivity : NSObject
+
+- (instancetype)initWithActivityType:(NSString *)activityType NS_DESIGNATED_INITIALIZER;
+- (instancetype)init;
+
+@property (readonly, copy) NSString *activityType;
+@property (nullable, copy) NSString *title;
+@property (nullable, copy) NSDictionary *userInfo;
+- (void)addUserInfoEntriesFromDictionary:(NSDictionary *)otherDictionary;
+@property (nullable, copy) NSSet<NSString *> *requiredUserInfoKeys;
+@property BOOL needsSave;
+@property (nullable, copy) NSURL *webpageURL;
+@property (nullable, copy) NSURL *referrerURL;
+@property (nullable, copy) NSDate *expirationDate;
+@property (copy) NSSet<NSString *> *keywords;
+@property BOOL supportsContinuationStreams;
+@property (nullable, weak) id<NSUserActivityDelegate> delegate;
+@property (nullable, copy) NSString *targetContentIdentifier;
+@property (nullable, copy) NSUserActivityPersistentIdentifier persistentIdentifier;
+
+@property (getter=isEligibleForHandoff) BOOL eligibleForHandoff;
+@property (getter=isEligibleForSearch) BOOL eligibleForSearch;
+@property (getter=isEligibleForPublicIndexing) BOOL eligibleForPublicIndexing;
+@property (getter=isEligibleForPrediction) BOOL eligibleForPrediction;
+
+- (void)becomeCurrent;
+- (void)resignCurrent;
+- (void)invalidate;
+
+- (void)getContinuationStreamsWithCompletionHandler:(void (^)(NSInputStream * _Nullable inputStream, NSOutputStream * _Nullable outputStream, NSError * _Nullable error))completionHandler;
+
 @end
+
+@protocol NSUserActivityDelegate <NSObject>
+@optional
+
+- (void)userActivityWillSave:(NSUserActivity *)userActivity;
+- (void)userActivityWasContinued:(NSUserActivity *)userActivity;
+- (void)userActivity:(NSUserActivity *)userActivity didReceiveInputStream:(NSInputStream *)inputStream outputStream:(NSOutputStream *)outputStream;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- src/NSUserActivity.m
+++ src/NSUserActivity.m
@@ -18,21 +18,217 @@
 */
 
 #import <Foundation/NSUserActivity.h>
-#import <Foundation/NSMethodSignature.h>
-#import <Foundation/NSInvocation.h>
+#import <Foundation/NSArray.h>
+#import <Foundation/NSBundle.h>
+#import <Foundation/NSDictionary.h>
+#import <Foundation/NSError.h>
+#import <Foundation/NSException.h>
+#import <Foundation/NSSet.h>
+#import <Foundation/NSString.h>
+#import <Foundation/FoundationErrors.h>
+#import <dispatch/dispatch.h>
+#import <objc/runtime.h>
 
 NSString * const NSUserActivityTypeBrowsingWeb = @"NSUserActivityTypeBrowsingWeb";
 
-@implementation NSUserActivity
+// The process-wide current activity; guarded by @synchronized([NSUserActivity class]).
+static NSUserActivity *currentActivity = nil;
 
-- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector
+@implementation NSUserActivity
 {
-    return [NSMethodSignature signatureWithObjCTypes: "v@:"];
+    id _delegate;
+    BOOL _needsSave;
+    BOOL _invalidated;
+    BOOL _saveScheduled;
 }
 
-- (void)forwardInvocation:(NSInvocation *)anInvocation
+@synthesize activityType = _activityType;
+@synthesize title = _title;
+@synthesize userInfo = _userInfo;
+@synthesize requiredUserInfoKeys = _requiredUserInfoKeys;
+@synthesize webpageURL = _webpageURL;
+@synthesize referrerURL = _referrerURL;
+@synthesize expirationDate = _expirationDate;
+@synthesize keywords = _keywords;
+@synthesize supportsContinuationStreams = _supportsContinuationStreams;
+@synthesize targetContentIdentifier = _targetContentIdentifier;
+@synthesize persistentIdentifier = _persistentIdentifier;
+@synthesize eligibleForHandoff = _eligibleForHandoff;
+@synthesize eligibleForSearch = _eligibleForSearch;
+@synthesize eligibleForPublicIndexing = _eligibleForPublicIndexing;
+@synthesize eligibleForPrediction = _eligibleForPrediction;
+
+- (instancetype)initWithActivityType:(NSString *)activityType
 {
-    NSLog(@"Stub called: %@ in %@", NSStringFromSelector([anInvocation selector]), [self class]);
+    if (activityType == nil)
+    {
+        [self release];
+        [NSException raise:NSInvalidArgumentException format:@"-[NSUserActivity initWithActivityType:] requires a non-nil activity type"];
+    }
+    self = [super init];
+    if (self != nil)
+    {
+        _activityType = [activityType copy];
+        _keywords = [[NSSet alloc] init];
+        _eligibleForHandoff = YES;
+    }
+    return self;
+}
+
+// Documented to use the first entry of the main bundle's NSUserActivityTypes.
+- (instancetype)init
+{
+    id types = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"NSUserActivityTypes"];
+    if (![types isKindOfClass:[NSArray class]] || [types count] == 0 || ![[types objectAtIndex:0] isKindOfClass:[NSString class]])
+    {
+        [self release];
+        [NSException raise:NSInternalInconsistencyException format:@"-[NSUserActivity init] requires an NSUserActivityTypes array in the main bundle's Info.plist; use -initWithActivityType:"];
+    }
+    return [self initWithActivityType:[types objectAtIndex:0]];
+}
+
+- (void)dealloc
+{
+    objc_storeWeak(&_delegate, nil);
+    [_activityType release];
+    [_title release];
+    [_userInfo release];
+    [_requiredUserInfoKeys release];
+    [_webpageURL release];
+    [_referrerURL release];
+    [_expirationDate release];
+    [_keywords release];
+    [_targetContentIdentifier release];
+    [_persistentIdentifier release];
+    [super dealloc];
+}
+
+- (void)addUserInfoEntriesFromDictionary:(NSDictionary *)otherDictionary
+{
+    @synchronized(self)
+    {
+        NSMutableDictionary *merged = [_userInfo mutableCopy] ?: [[NSMutableDictionary alloc] init];
+        [merged addEntriesFromDictionary:otherDictionary];
+        [_userInfo release];
+        _userInfo = [merged copy];
+        [merged release];
+    }
+}
+
+- (id<NSUserActivityDelegate>)delegate
+{
+    return objc_loadWeak(&_delegate);
+}
+
+- (void)setDelegate:(id<NSUserActivityDelegate>)delegate
+{
+    objc_storeWeak(&_delegate, delegate);
+}
+
+- (BOOL)needsSave
+{
+    @synchronized(self)
+    {
+        return _needsSave;
+    }
+}
+
+- (void)setNeedsSave:(BOOL)needsSave
+{
+    @synchronized(self)
+    {
+        _needsSave = needsSave;
+    }
+    if (needsSave)
+        [self _scheduleSaveIfCurrent];
+}
+
+- (BOOL)_isCurrent
+{
+    @synchronized([NSUserActivity class])
+    {
+        return currentActivity == self;
+    }
+}
+
+// Repeated needsSave requests coalesce into one save on the main queue.
+- (void)_scheduleSaveIfCurrent
+{
+    @synchronized(self)
+    {
+        if (!_needsSave || _invalidated || _saveScheduled || ![self _isCurrent])
+            return;
+        _saveScheduled = YES;
+    }
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self _save];
+    });
+}
+
+- (void)_save
+{
+    @synchronized(self)
+    {
+        _saveScheduled = NO;
+        if (!_needsSave || _invalidated || ![self _isCurrent])
+            return;
+    }
+    @autoreleasepool
+    {
+        id<NSUserActivityDelegate> delegate = self.delegate;
+        if ([delegate respondsToSelector:@selector(userActivityWillSave:)])
+            [delegate userActivityWillSave:self];
+    }
+    self.needsSave = NO;
+}
+
+- (void)becomeCurrent
+{
+    NSUserActivity *previous;
+    @synchronized(self)
+    {
+        if (_invalidated)
+            return;
+        @synchronized([NSUserActivity class])
+        {
+            previous = currentActivity;
+            currentActivity = [self retain];
+        }
+    }
+    [previous release];
+    [self _scheduleSaveIfCurrent];
+}
+
+- (void)resignCurrent
+{
+    BOOL wasCurrent = NO;
+    @synchronized([NSUserActivity class])
+    {
+        if (currentActivity == self)
+        {
+            currentActivity = nil;
+            wasCurrent = YES;
+        }
+    }
+    if (wasCurrent)
+        [self release];
+}
+
+- (void)invalidate
+{
+    @synchronized(self)
+    {
+        _invalidated = YES;
+    }
+    [self resignCurrent];
+}
+
+// Darling has no Handoff service, so no activity is ever continued from another device.
+- (void)getContinuationStreamsWithCompletionHandler:(void (^)(NSInputStream *, NSOutputStream *, NSError *))completionHandler
+{
+    NSDictionary *info = @{ NSLocalizedDescriptionKey: @"Continuation streams are unavailable: Handoff is not supported on this system." };
+    NSError *error = [NSError errorWithDomain:NSCocoaErrorDomain code:NSFeatureUnsupportedError userInfo:info];
+    completionHandler(nil, nil, error);
 }
 
 @end
